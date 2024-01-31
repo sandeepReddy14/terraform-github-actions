22b35dfe5872b36fdb9a8bac9a7b3c7e58e7d626
sandeepReddy14 <sandeepreddybalireddy@gmail.com>
2024-02-01 02:58:38 +0530

    Added resource group.

--- deploy/main.tf
+++ deploy/main.tf
@@ -5,3 +5,8 @@ provider "azurerm" {
 provider "databricks" {
   azure_workspace_resource_id = azurerm_databricks_workspace.intact_databricks_workspace.id
 }
+
+resource "azurerm_resource_group" "intact_resource_group" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
