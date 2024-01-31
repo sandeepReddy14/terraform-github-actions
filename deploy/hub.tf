6e3a3973041fde678b232e59650956e2bf15b92c
sandeepReddy14 <sandeepreddybalireddy@gmail.com>
2024-02-01 03:00:07 +0530

    Added depends on for hub vnet and subnet

--- deploy/hub.tf
+++ deploy/hub.tf
@@ -11,6 +11,7 @@ resource "azurerm_virtual_network" "intact_hub_virtual_network" {
     tags = {
         environment = var.environment
     }
+    depends_on = [ azurerm_resource_group.intact_resource_group ]
 }
 
 # subnet in hub
@@ -20,4 +21,5 @@ resource "azurerm_subnet" "intact_hub_sub_net" {
     resource_group_name = var.resource_group_name
     virtual_network_name = azurerm_virtual_network.intact_hub_virtual_network.name
     address_prefixes = var.hub_subnet_address_prefixes
+    depends_on = [ azurerm_virtual_network.intact_hub_virtual_network ]
 }
